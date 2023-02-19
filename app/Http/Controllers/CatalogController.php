f9481a9346d1840c025b7157db15b12225afbbbb
Felipe Pastana <felipeapastana@gmail.com>
2023-02-18 23:20:59 -0700

    Update CatalogController.php

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -10,11 +10,6 @@ use App\Category;
 
 class CatalogController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('catalog.index');
@@ -29,22 +24,6 @@ class CatalogController extends Controller
         return $products;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
 
@@ -62,12 +41,6 @@ class CatalogController extends Controller
         return 'Product inserted into database';
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Catalog  $catalog
-     * @return \Illuminate\Http\Response
-     */
     public function show(Request $request)
     {
         if(isset($request->sku)){
@@ -82,41 +55,12 @@ class CatalogController extends Controller
 
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Catalog  $catalog
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Catalog $catalog)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Catalog  $catalog
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Catalog $catalog)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Catalog  $catalog
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Request $request)
     {
         if(isset($request->sku)){
             $delete_product = Product::where('sku', $request->sku)
             ->delete();
-            if($delete_product == true){
+            if($delete_product === true){
                 return 'Item deleted from database';
             } else {
                 return 'Item was not found in the database';
